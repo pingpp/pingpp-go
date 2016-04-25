98dafe8773a18d9e34671aacce01ae5b6bd50790
ghbai <guanghe.bai@pingxx.com>
2016-04-25 16:57:53 +0800

    为方便http接口测试返回一个http.ResponseWriter对象

---
+++ utils/fake/fakeresponse.go
@@ -0,0 +1,36 @@
+package fake
+
+import "net/http"
+
+type FakeResponse struct {
+	headers http.Header
+	body    []byte
+	status  int
+}
+
+func NewFakeResponse() *FakeResponse {
+	return &FakeResponse{
+		headers: make(http.Header),
+	}
+}
+
+func (r *FakeResponse) Header() http.Header {
+	return r.headers
+}
+
+func (r *FakeResponse) Write(body []byte) (int, error) {
+	r.body = body
+	return len(body), nil
+}
+
+func (r *FakeResponse) WriteHeader(status int) {
+	r.status = status
+}
+
+func (r *FakeResponse) Body() []byte {
+	return r.body
+}
+
+func (r *FakeResponse) Clear() {
+	r.body = []byte{}
+}
